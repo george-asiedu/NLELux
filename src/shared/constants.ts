7f1b4ff2bd00da4ce0a924e496d9e858ac340be6
george-asiedu <asiedug41@gmail.com>
2025-06-26 07:17:39 +0000

    app constants defied for swagger, allowed origins, allowed headers, and allowed methods

---
+++ src/shared/constants.ts
@@ -0,0 +1,22 @@
+export const swagger = {
+  swaggerDocsTitle: 'ERP System API Documentation',
+  swaggerDocsDescription:
+    'REST API for NLELux ecommerce platform, providing endpoints for managing products, orders, users, and more.',
+  swaggerDocsVersion: '1.0.0',
+  swaggerDocsPath: 'api-docs',
+  productionUrl: 'https://nle_lux.onrender.com',
+  localUrl: 'http://localhost:',
+  globalPrefix: 'api/v1',
+  noAccess: 'Url cannot access this resource',
+};
+
+export const allowedOrigins = ['http://localhost:4200'];
+export const allowedMethods = [
+  'GET',
+  'POST',
+  'PUT',
+  'DELETE',
+  'PATCH',
+  'OPTIONS',
+];
+export const allowedHeaders = 'Content-Type, Accept, X-Powered-By: NLELux';
