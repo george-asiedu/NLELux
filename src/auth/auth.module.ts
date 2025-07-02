099b4158bcb4cbed22b434de105ab441fd1cbb76
george-asiedu <asiedug41@gmail.com>
2025-07-02 09:11:26 +0000

    pisma module, mai module, and jwt module registered in auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,27 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './service/auth.service';
 import { AuthController } from './controller/auth.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { MailModule } from '../mail/mail.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    MailModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRY', '30m'),
+        },
+      }),
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
+  exports: [JwtModule],
 })
 export class AuthModule {}
