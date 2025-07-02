59fdce7ec34ee628aa512fda4a61bc1f37c9268b
george-asiedu <asiedug41@gmail.com>
2025-07-02 09:39:45 +0000

    signupResponse, and signupBadRequest swagger types added

---
+++ src/shared/utils/swagger.utils.ts
@@ -0,0 +1,25 @@
+export const signupResponse = {
+  status: 201,
+  message: 'User signed up successfully',
+  data: {
+    user: {
+      id: '6864f9891a83097e94c27686',
+      email: 'resiw33129@ofacer.com',
+      name: 'Kanan Stark',
+    },
+    token:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJlc2l3MzMxMjlAb2ZhY2VyLmNvbSIsImNvZGUiOiIzNTQxODYiLCJ0eXBlIjoic2lnbnVwIiwiaWF0IjoxNzUxNDQ3OTQ2LCJleHAiOjE3NTE0NTE1NDZ9.l4Q9cEnU2I64m__PzLuIKK0ynNFHYI3GQ1rIfazVLI0',
+  },
+};
+
+export const signupBadRequest = {
+  message: [
+    'name - Name must contain only letters and single spaces between words, no numbers or special characters allowed',
+    'email - Please provide a valid email address',
+    'email - Email must be a valid format with only allowed characters (letters, numbers, dots, underscores, hyphens)',
+    'password - Password must be at least 8 characters long',
+    'password - Password cannot exceed 50 characters',
+  ],
+  error: 'Bad Request',
+  statusCode: 400,
+};
