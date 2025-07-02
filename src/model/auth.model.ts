a74a91702b6faa4ce2d156560d33f248bdb753f6
george-asiedu <asiedug41@gmail.com>
2025-07-02 10:40:52 +0000

    reponse, request and jwt payload interfaces defined

--- src/model/auth.model.ts
+++ src/model/auth.model.ts
@@ -1,5 +1,22 @@
+import { User } from '@prisma/client';
+import { Request } from 'express';
+
 export interface JwtTokenPayload {
   email: string;
   code: string;
   type: 'signup' | 'access' | 'refresh';
+}
+
+export interface JwtPayload {
+  userId: string;
+}
+
+export interface RequestInterface extends Request {
+  user: User;
+}
+
+export interface ResponseInterface<T> {
+  status: number;
+  message: string;
+  data: T;
 }
