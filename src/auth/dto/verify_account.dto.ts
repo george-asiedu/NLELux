9399aaafada8e847c8212c8899fe47574f2e483d
george-asiedu <asiedug41@gmail.com>
2025-07-02 10:39:17 +0000

    verifyAccountSchema defined

---
+++ src/auth/dto/verify_account.dto.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+import { patterns, validations } from '../../shared/constants';
+
+export const verifyAccountSchema = z
+  .object({
+    code: z
+      .string({ required_error: validations.codeRequired })
+      .regex(patterns.code, validations.codeFormatError)
+      .trim(),
+  })
+  .required();
+
+export type verifyAccountDto = z.infer<typeof verifyAccountSchema>;
