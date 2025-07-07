13c34d941fba807545a4e936c07ac9768ad091c0
george-asiedu <asiedug41@gmail.com>
2025-07-07 09:54:31 +0000

    passwordsMatch function declared

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,6 +1,11 @@
 import { z } from 'zod';
 import { patterns, validations } from '../../shared/constants';
 
+const passwordsMatch = (data: {
+  password: string;
+  confirmPassword: string;
+}): boolean => data.password === data.confirmPassword;
+
 export const signupSchema = z
   .object({
     name: z
@@ -22,7 +27,16 @@ export const signupSchema = z
       .min(8, validations.passwordLengthError)
       .max(50, validations.passwordLengthError)
       .regex(patterns.password, validations.passwordMaxLengthError),
+
+    confirmPassword: z
+      .string({ required_error: validations.passwordRequired })
+      .min(8, validations.passwordLengthError)
+      .max(50, validations.passwordLengthError)
+      .regex(patterns.password, validations.passwordMaxLengthError),
   })
-  .required();
+  .refine(passwordsMatch, {
+    message: validations.passwordsDoNotMatch,
+    path: ['confirmPassword'],
+  });
 
 export type signupDto = z.infer<typeof signupSchema>;
