aaaaf8d67a1e413349d65ebfa1266809f7ab1843
george-asiedu <asiedug41@gmail.com>
2025-07-07 12:19:50 +0000

    auth route paths defined

---
+++ src/shared/utils/routes.ts
@@ -0,0 +1,9 @@
+export const AuthRoutes = {
+  signup: 'signup',
+  verifyAccount: 'verify-account',
+  login: 'login',
+  refreshToken: 'refresh-token',
+  forgotPassword: 'forgot-password',
+  resetPassword: 'reset-password',
+  verificationEmail: 'verification-email',
+};
