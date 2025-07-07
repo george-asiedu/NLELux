a7de7b7c679f62df941ae1c3627db99c746cf2c7
george-asiedu <asiedug41@gmail.com>
2025-07-07 14:01:41 +0000

    logger property added

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,9 @@ import { Request, Response } from 'express';
 import { NotFoundException } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'debug', 'verbose'],
+  });
   app.enableCors({
     origin: allowedOrigins,
     methods: allowedMethods,
