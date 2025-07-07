75b4b142739d1a587968eb65f31c1a8e757ed5a0
george-asiedu <asiedug41@gmail.com>
2025-07-07 09:38:45 +0000

    privileges imported to acess role keys

--- src/shared/guards/roles.decorator.ts
+++ src/shared/guards/roles.decorator.ts
@@ -1,6 +1,6 @@
 import { Role } from '@prisma/client';
-import { validations } from '../constants';
+import { privileges } from '../constants';
 import { SetMetadata } from '@nestjs/common';
 
-const role_keys = validations.rolesKey;
+const role_keys: string = privileges.rolesKey;
 export const role = (...roles: Role[]) => SetMetadata(role_keys, roles);
