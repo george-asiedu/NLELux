1b97fca0db7bb40bca1d0e08c2a23ed511ac2ca5
george-asiedu <asiedug41@gmail.com>
2025-07-27 08:58:37 +0000

    auth interfaces defined

---
+++ src/shared/interfaces/auth.model.ts
@@ -0,0 +1,58 @@
+import { AccountStatus, Role } from '@prisma/client';
+
+export enum AuthToken {
+  ACCESS = 'access-tk',
+  REFRESH_TOKEN = 'refresh-tk',
+}
+
+export interface Data<T> {
+  message: string;
+  data: T;
+}
+
+export interface MessageOnly {
+  message: string;
+}
+
+export interface RefreshTokenPayload {
+  sub: string;
+  email: string;
+  token: AuthToken;
+  exp: number;
+  iat: number;
+}
+
+export interface UserInfo {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  role: Role;
+  accountStatus?: AccountStatus;
+}
+
+export interface JwtTokenPayload {
+  sub: string;
+  token: AuthToken;
+  exp: number;
+  iat: number;
+}
+
+export interface AuthRequestProps {
+  headers: {
+    authorization?: string;
+    [key: string]: any;
+  };
+  user?: {
+    id: string;
+    email: string;
+    name: string;
+    role: string;
+  };
+  [key: string]: any;
+}
+
+export interface VerifyEmailProps {
+  code: string;
+  expirationTime?: string;
+}
