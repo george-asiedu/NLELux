4ae3f8660c0a4dafa5b110b6a4861ca03ba1ef7f
george-asiedu <asiedug41@gmail.com>
2025-08-05 07:50:37 +0000

    messages modified to use trendloom

--- src/shared/utils/constants.ts
+++ src/shared/utils/constants.ts
@@ -1,25 +1,14 @@
 export const swagger = {
-  swaggerDocsTitle: 'NLELux API Documentation',
+  swaggerDocsTitle: 'TrendLoom API Documentation',
   swaggerDocsDescription:
-    'REST API for NLELux ecommerce platform, providing endpoints for managing products, orders, users, and more.',
+    'REST API for TrendLoom ecommerce platform, providing endpoints for managing products, orders, users, and more.',
   swaggerDocsVersion: '1.0.0',
   swaggerDocsPath: 'api-docs',
   productionUrl: 'https://nle_lux.onrender.com',
   localUrl: 'http://localhost:',
-  globalPrefix: 'api/v1',
+  globalPrefix: 'api',
   noAccess: 'Url cannot access this resource',
 };
-
-export const allowedOrigins = ['http://localhost:4200'];
-export const allowedMethods = [
-  'GET',
-  'POST',
-  'PUT',
-  'DELETE',
-  'PATCH',
-  'OPTIONS',
-];
-export const allowedHeaders = 'Content-Type, Accept, X-Powered-By: NLELux';
 
 export const validations = {
   validationError: 'Validation failed',
