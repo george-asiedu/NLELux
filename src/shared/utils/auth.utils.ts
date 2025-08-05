37240d63009a5bd4d077782118188d6168e44873
george-asiedu <asiedug41@gmail.com>
2025-08-05 07:50:18 +0000

    createLoginToken method moved to auth utils file

--- src/shared/utils/auth.utils.ts
+++ src/shared/utils/auth.utils.ts
@@ -1,7 +1,7 @@
 import * as bcrypt from 'bcrypt';
-import { PrismaService } from '../../prisma/prisma.service';
-import { ConflictException, NotFoundException } from '@nestjs/common';
-import { errorMessages, validations } from './constants';
+import { AuthToken, UserInfo } from '../interfaces/auth.model';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 export const hashPassword = async (password: string) => {
   const salt = await bcrypt.genSalt(10);
@@ -19,32 +19,31 @@ export const generateCode = (): string => {
   return Math.floor(100000 + Math.random() * 900000).toString();
 };
 
-export const existingUser = async (prisma: PrismaService, email: string) => {
-  const existingUser = await prisma.user.findUnique({
-    where: { email: email.toLowerCase() },
-  });
-  if (existingUser) throw new ConflictException(validations.userExits);
-  return existingUser;
-};
+export const createLoginToken = (
+  user: UserInfo,
+  jwtService: JwtService,
+  configService: ConfigService,
+) => {
+  let secretKey = configService.get<string>('JWT_SECRET');
+  const accessTokenDuration = '30m';
 
-export const findUserByEmail = async (prisma: PrismaService, email: string) => {
-  const foundUser = await prisma.user.findUnique({
-    where: { email: email.toLowerCase() },
-    select: {
-      id: true,
-      name: true,
-      email: true,
-      role: true,
-      accountStatus: true,
-      password: true,
+  const accessToken = jwtService.sign(
+    {
+      sub: user.id,
+      token: AuthToken.ACCESS,
     },
-  });
-  if (!foundUser) throw new NotFoundException(errorMessages.userNotFound);
-  return foundUser;
-};
+    { expiresIn: accessTokenDuration, secret: secretKey },
+  );
 
-export const findUserByID = async (prisma: PrismaService, id: string) => {
-  const foundUser = await prisma.user.findUnique({ where: { id } });
-  if (!foundUser) throw new NotFoundException(errorMessages.userNotFound);
-  return foundUser;
+  const refreshTokenDuration = '7d';
+  const refreshToken = jwtService.sign(
+    {
+      sub: user.id,
+      email: user.email,
+      token: AuthToken.REFRESH_TOKEN,
+    },
+    { expiresIn: refreshTokenDuration, secret: secretKey },
+  );
+
+  return { accessToken, refreshToken };
 };
