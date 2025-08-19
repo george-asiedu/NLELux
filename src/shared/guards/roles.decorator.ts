1ddce9fab2e5426e5f75a296b16bd80eba08faf1
george-asiedu <asiedug41@gmail.com>
2025-08-19 08:40:14 +0000

    role variable updated to Roles

--- src/shared/guards/roles.decorator.ts
+++ src/shared/guards/roles.decorator.ts
@@ -3,4 +3,4 @@ import { privileges } from '../utils/constants';
 import { SetMetadata } from '@nestjs/common';
 
 const role_keys: string = privileges.rolesKey;
-export const role = (...roles: Role[]) => SetMetadata(role_keys, roles);
+export const Roles = (...roles: Role[]) => SetMetadata(role_keys, roles);
